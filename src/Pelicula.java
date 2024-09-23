81b6b8bbc1bd26ec2377fb8583eb32c2af1480f8
JoseAusejo <joseantonioausejotoribio@gmail.com>
2024-09-23 09:36:46 -0500

    subita

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class Pelicula {
+     String nombre;
+
+     int fechaDeLanzamiento;
+     int duracionEnMinutos;
+
+     boolean incluidoEnElPlan;
+     private  double sumaDeLasEvaluaciones;
+     private int totalDelasEvaluaciones;
+
+     int getTotalDelasEvaluaciones(){
+          return totalDelasEvaluaciones;
+     }
+     void  muestraFichaTecnica() {
+          System.out.println("El nombre de la pelicula es : " + nombre);
+          System.out.println("Su fecha de lanzamiento es : " + fechaDeLanzamiento);
+          System.out.println("Duracion en minutos: " + duracionEnMinutos);
+     }
+     void  evalua(double nota){
+          sumaDeLasEvaluaciones += nota;
+          totalDelasEvaluaciones ++;
+     }
+     double calculaMedia(){
+          return sumaDeLasEvaluaciones / totalDelasEvaluaciones;
+     }
+
 }
